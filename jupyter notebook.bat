c04b11ee24485635127816b7675867b70697bd45
Tomas Segade <tomy.segade@gmail.com>
2024-02-21 17:06:23 -0300

    Sistemas terminados

---
+++ jupyter notebook.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Este script ejecutará Jupyter Notebook
+
+REM Asegúrate de que la carpeta de instalación de Python esté en el PATH
+REM Reemplaza 'python' con 'python3' si es necesario
+python -m notebook
